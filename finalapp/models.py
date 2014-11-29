199855abb4c34fea623ba01cf2ae3a905b8ea7d8
Akash Agrawall <akash.wanted@gmail.com>
2014-11-29 20:12:49 +0530

    save it

--- finalapp/models.py
+++ finalapp/models.py
@@ -5,10 +5,39 @@ class Container(models.Model):
     uid=models.CharField(max_length=200)
     name_image=models.CharField(max_length=200)
     id_container=models.CharField(max_length=200)
+    created = models.CharField(max_length=200)
+    os = models.CharField(max_length=200)
+    virual_size = models.CharField(max_length=200)
 
 class Image(models.Model):
     uid=models.CharField(max_length=200)
     name=models.CharField(max_length=200)
     id_images=models.CharField(max_length=200)
+    virtual_size = models.CharField(max_length=200)
+    repo_tags = models.CharField(max_length=200)
+    created = models.CharField(max_length=200)
+    os = models.CharField(max_length=200)
+
+
+class User_Image(models.Model):
+    uid=models.CharField(max_length=200)
+    name=models.CharField(max_length=200)
+    id_images=models.CharField(max_length=200)
+    virtual_size = models.CharField(max_length=200)
+    repo_tags = models.CharField(max_length=200)
+    created = models.CharField(max_length=200)
+    os = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    cmd = models.CharField(max_length=200)
+
+class pushed_images(models.Model):
+    uid=models.CharField(max_length=200)
+    name=models.CharField(max_length=200)
+    id_images=models.CharField(max_length=200)
+    virtual_size = models.CharField(max_length=200)
+    repo_tags = models.CharField(max_length=200)
+    created = models.CharField(max_length=200)
+    os = models.CharField(max_length=200)
+
 
 # Create your models here.
