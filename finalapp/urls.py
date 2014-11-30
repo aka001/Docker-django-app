3da635f275b8bd51b134099a39ce89bfb3289eb8
Akash Agrawall <akash.wanted@gmail.com>
2014-11-30 13:36:00 +0530

    final project

--- finalapp/urls.py
+++ finalapp/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
 	url(r'^ask_stop_container/$',views.ask_stop_container,name='ask_stop_container'),
 	url(r'^stop_container/(?P<container_id>\S+)/$',views.stop_container,name='stop_container'),
 
+	url(r'^deploy_war_app/(?P<ip_app>\S+)/$',views.deploy_war_app,name='deploy_war_app'),
+
 	url(r'^ask_del_container/$',views.ask_del_container,name='ask_del_container'),
 	url(r'^delete_container/(?P<container_id>\S+)/$',views.delete_container,name='delete_container'),
 
@@ -21,6 +23,10 @@ urlpatterns = [
 
 	url(r'^ask_search_images/$',views.ask_search_images,name='ask_search_images'),
 	url(r'^search_images/(?P<search_name>\S+)/$',views.search_images,name='search_images'),
+
+	url(r'^ask_del_commited_images/$',views.ask_del_commited_images,name='ask_del_commited_images'),
+	url(r'^delete_commited_images/(?P<idd>\S+)/$',views.delete_commited_images,name='delete_commited_images'),
+
 	url(r'^ask_del_images/$',views.ask_del_images,name='ask_del_images'),
 	url(r'^delete_images/(?P<idd>\S+)/$',views.delete_images,name='delete_images'),
 	url(r'^view_all_images/$',views.view_all_images,name='view_all_images'),
